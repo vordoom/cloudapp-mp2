1bc65d86af4a158b42123ce888d06128efd2e0ae
Sergey Mudrov <vordoom@hotmail.com>
2015-09-25 01:36:35 +0300

    Orphan pages solution

--- OrphanPages.java
+++ OrphanPages.java
@@ -13,7 +13,10 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 // >>> Don't Change
 public class OrphanPages extends Configured implements Tool {
@@ -26,20 +29,69 @@ public class OrphanPages extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        //TODO
+        Job job = Job.getInstance(this.getConf(), "Title Count");
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(LinkCountMap.class);
+        job.setReducerClass(OrphanPageReduce.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(OrphanPages.class);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
+        String delimiters;
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            this.delimiters = getRegexDelimiters(" \t,;:.?!-:@[](){}_*/");
+        }
+
+        private String getRegexDelimiters(String value){
+            StringBuilder regexp = new StringBuilder("");
+            regexp.append("[");
+
+            for (int i = 0; i < value.length(); i++) {
+                regexp.append(Pattern.quote(
+                        Character.toString(value.charAt(i))
+                ));
+            }
+            regexp.append("]");
+
+            return regexp.toString();
+        }
+
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            //TODO
+            String line = value.toString().toLowerCase().trim();
+            String[] result = line.split(this.delimiters);
+
+            for(String s : result) {
+                if (s == null || s.length() == 0)
+                    continue;
+
+                context.write(new IntWritable(Integer.parseInt(s)), new IntWritable(1));
+            }
         }
     }
 
     public static class OrphanPageReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
         @Override
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //TODO
+            int total = 0;
+
+            for (IntWritable val : values)
+                total += val.get();
+
+            if (total <= 1)
+                context.write(key, NullWritable.get());
         }
     }
 }
