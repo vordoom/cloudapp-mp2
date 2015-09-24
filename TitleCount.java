42492102e828aefad4c9b19b3d8739bf8d48701a
Sergey Mudrov <vordoom@hotmail.com>
2015-09-24 23:24:14 +0300

    Title count solution

--- TitleCount.java
+++ TitleCount.java
@@ -14,14 +14,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 // >>> Don't Change
